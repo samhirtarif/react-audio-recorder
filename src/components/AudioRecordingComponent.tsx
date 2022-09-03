8b905f9b6c19cc4c4f524ca34a7f1f5779ab82ee
samhirtarif <samhir.tarif@gmail.com>
2022-09-03 13:53:22 +0500

    Extract interfaces to separate file

--- src/components/AudioRecordingComponent.tsx
+++ src/components/AudioRecordingComponent.tsx
@@ -7,55 +7,10 @@ import {
   faSave,
   faPlay,
 } from "@fortawesome/free-solid-svg-icons";
-
-import useAudioRecorder, { recorderControls } from "../hooks/useAudioRecorder";
+import { Props } from "./interfaces";
+import useAudioRecorder from "../hooks/useAudioRecorder";
 
 import "../styles/audio-recorder.css";
-
-interface StyleProps {
-  /**
-   * Applies passed classes to audio recorder container
-   **/
-  AudioRecorderClass?: string;
-  /**
-   * Applies passed classes to audio recorder start/save option
-   **/
-  AudioRecorderStartSaveClass?: string;
-  /**
-   * Applies passed classes to audio recorder timer
-   **/
-  AudioRecorderTimerClass?: string;
-  /**
-   * Applies passed classes to audio recorder status option
-   **/
-  AudioRecorderStatusClass?: string;
-  /**
-   * Applies passed classes to audio recorder pause/resume option
-   **/
-  AudioRecorderPauseResumeClass?: string;
-  /**
-   * Applies passed classes to audio recorder discard option
-   **/
-  AudioRecorderDiscardClass?: string;
-}
-
-interface Props {
-  /**
-   * This gets called when the save button is clicked.
-   * In case the recording is cancelled, the blob is discarded.
-   **/
-  onRecordingComplete?: (blob: Blob) => void;
-  /**
-   * Allows calling of hook outside this component. The controls returned by the hook can then be passed to the component using this prop.
-   * This allows for use of hook methods and state outside this component
-   * @sample_usage https://github.com/samhirtarif/react-audio-recorder#combine-the-useaudiorecorder-hook-and-the-audiorecorder-component
-   **/
-  recorderControls?: recorderControls;
-  /**
-   * Custom classes to changes styles.
-   **/
-  classes?: StyleProps;
-}
 
 /**
  * Usage: https://github.com/samhirtarif/react-audio-recorder#audiorecorder-component
