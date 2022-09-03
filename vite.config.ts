ba3c726617397e3b8e469aa368dcf74daae94821
samhirtarif <samhir.tarif@gmail.com>
2022-09-04 03:22:43 +0500

    Finalize ready for publish

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+// import libCss from 'vite-plugin-libcss';
+import dts from 'vite-plugin-dts';
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [
+    cssInjectedByJsPlugin(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.tsx'),
