ab9f6ce0140e28336049cf040022e547e571e89b
Samhir Tarif <89391183+samhirtarif@users.noreply.github.com>
2023-04-16 13:37:38 +0300

    Resolve nextjs error (#26)
    * Resolve 'document is undefined' error for nextJS
    
    * Make styles more robust
    
    * Updated package version to 1.0.10

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,22 @@ import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 import dts from 'vite-plugin-dts';
 import path from 'path'
 
+const injectCodeFunction = (cssCode) => {
+  try {
+    if (typeof window === 'undefined') return;
+
+    var elementStyle = document.createElement('style');
+    elementStyle.appendChild(document.createTextNode(cssCode));
+    document.head.appendChild(elementStyle);
+  } catch (e) {
+    console.error('vite-plugin-css-injected-by-js', e);
+  }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    cssInjectedByJsPlugin(),
+    cssInjectedByJsPlugin({ injectCodeFunction }),
     dts({
       insertTypesEntry: true,
     }),
